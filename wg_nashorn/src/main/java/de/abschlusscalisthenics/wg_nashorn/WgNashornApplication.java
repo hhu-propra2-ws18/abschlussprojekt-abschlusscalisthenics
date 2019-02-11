21cff32180b3ea3e60b9b35b5c45cb7f27e42eca
serwanj <Serwan.Jassim@hhu.de>
2019-02-11 13:05:53 +0100

    Create gradle project

---
+++ wg_nashorn/src/main/java/de/abschlusscalisthenics/wg_nashorn/WgNashornApplication.java
@@ -0,0 +1,14 @@
+package de.abschlusscalisthenics.wg_nashorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WgNashornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WgNashornApplication.class, args);
+	}
+
+}
+
---
+++ wg_nashorn/src/test/java/de/abschlusscalisthenics/wg_nashorn/WgNashornApplicationTests.java
@@ -0,0 +1,17 @@
+package de.abschlusscalisthenics.wg_nashorn;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WgNashornApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
