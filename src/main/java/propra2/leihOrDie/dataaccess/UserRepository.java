1f354d72b17edd91063c84439645f1814171e142
serwanj <Serwan.Jassim@hhu.de>
2019-02-14 11:21:17 +0100

    Update UserRepository with Query to find User by EMail Address

--- src/main/java/propra2/leihOrDie/dataaccess/UserRepository.java
+++ src/main/java/propra2/leihOrDie/dataaccess/UserRepository.java
@@ -1,10 +1,15 @@
 package propra2.leihOrDie.dataaccess;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import propra2.leihOrDie.model.User;
 
 import java.util.List;
 
 public interface UserRepository extends CrudRepository<User, String> {
     List<User> findAll();
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    List<User> findUserByEMail(@Param("email") String email);
 }
