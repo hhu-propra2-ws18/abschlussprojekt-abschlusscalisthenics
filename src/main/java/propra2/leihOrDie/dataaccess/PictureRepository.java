fd416e53db53848567bfa58700e70584f9bac200
serwanj <Serwan.Jassim@hhu.de>
2019-02-14 13:22:29 +0100

    Adjust query WHERE statement

--- src/main/java/propra2/leihOrDie/dataaccess/PictureRepository.java
+++ src/main/java/propra2/leihOrDie/dataaccess/PictureRepository.java
@@ -10,6 +10,6 @@ import java.util.List;
 public interface PictureRepository extends CrudRepository<Picture, Long> {
     List<Picture> findAll();
 
-    @Query("SELECT p FROM Picture p WHERE p.item.item_id = :itemId")
+    @Query("SELECT p FROM Picture p WHERE p.item.id = :itemId")
     List<Picture> findPicturesOfItem(@Param("itemId") Long itemId);
 }
