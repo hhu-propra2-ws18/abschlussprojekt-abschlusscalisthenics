20833f63338d5179361fc44337fbb5018ec0f852
serwanj <Serwan.Jassim@hhu.de>
2019-02-14 13:11:21 +0100

    Delete blob attribute in picture entity

--- src/main/java/propra2/leihOrDie/model/Picture.java
+++ src/main/java/propra2/leihOrDie/model/Picture.java
@@ -2,26 +2,41 @@ package propra2.leihOrDie.model;
 
 import lombok.Data;
 import javax.persistence.*;
-import javax.sql.rowset.serial.SerialBlob;
+import java.io.Serializable;
 
 @Data
 @Entity
+@IdClass(PrimaryKey.class)
 public class Picture {
+    @Id
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
     @Id
     @GeneratedValue
     @Column(name="picture_id")
     private Long id;
 
-    private SerialBlob blob;
-
-    @ManyToOne
-    @JoinColumn(name="item_id")
-    private Item item;
-
     public Picture() {}
 
-    public Picture(SerialBlob blob, Item item) {
-        this.blob = blob;
+    public Picture(Item item) {
+        /*if(id >= 10) {
+            throw Exception;
+        }*/
+
+        this.item = item;
+    }
+}
+
+class PrimaryKey implements Serializable {
+    protected Long id;
+
+    protected Item item;
+
+    public PrimaryKey() {}
+
+    public PrimaryKey(Item item) {
         this.item = item;
     }
 }
