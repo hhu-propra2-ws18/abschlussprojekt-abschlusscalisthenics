6a7fd6d8894e3ff3699db2e052fb8a3d01ac2c9b
mofoku <lata222@gmail.com>
2019-02-15 21:07:43 +0100

    Fixed Typo.

--- src/test/java/propra2/leihOrDie/DummyItemGenerator.java
+++ src/test/java/propra2/leihOrDie/DummyItemGenerator.java
@@ -1,12 +1,13 @@
 package propra2.leihOrDie;
 
+import propra2.leihOrDie.model.Address;
 import propra2.leihOrDie.model.Item;
 import propra2.leihOrDie.model.User;
 
 public class DummyItemGenerator {
     public DummyItemGenerator() { }
 
-    public Item generatItem(User user) {
+    public Item generateItem(User user) {
         String name= "Fahrrad";
         String description = "Stereo Hybrid 160 Action Team 500Wh (2018)";
         int cost = 50;
@@ -19,7 +20,7 @@ public class DummyItemGenerator {
         return item;
     }
 
-    public Item generatAnotherItem(User user) {
+    public Item generateAnotherItem(User user) {
         String name= "Kickbike";
         String description = "Kickbike Cross MAX 29er – Offroad. Farbe: Schwartz";
         int cost = 80;
