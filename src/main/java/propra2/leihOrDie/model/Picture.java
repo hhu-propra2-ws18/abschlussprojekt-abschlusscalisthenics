61e82e9e50099dc6a8e671ef16778bb2601accce
serwanj <Serwan.Jassim@hhu.de>
2019-02-15 15:00:19 +0100

    Remove location from Item entity and only use picure_id in Picture entity as primary key

--- src/main/java/propra2/leihOrDie/model/Picture.java
+++ src/main/java/propra2/leihOrDie/model/Picture.java
@@ -7,33 +7,22 @@ import java.io.Serializable;
 
 @Data
 @Entity
-@IdClass(Picture.ComposedKey.class)
+//@IdClass(PictureKey.class)
 public class Picture {
-    @Id
     @ManyToOne
-    @GeneratedValue(strategy = GenerationType.AUTO)
     @JoinColumn(name="item_id")
     private Item item;
 
     @Id
     @Column(name="picture_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     public Picture() {}
 
     public Picture(Item item) {
         this.item = item;
-    }
-
-    static class ComposedKey implements Serializable {
-        protected Long id;
-
-        protected Item item;
-
-        public ComposedKey() {}
-
-        public ComposedKey(Item item) {
-            this.item = item;
-        }
+        //this.composedKey = new ComposedKey(item);
     }
 }
+
