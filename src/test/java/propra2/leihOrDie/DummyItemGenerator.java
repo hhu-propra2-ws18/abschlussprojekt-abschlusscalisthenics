ee273bfd0ff5da7f056bcf63d7dd1d2ef0d28b1b
mofoku <lata222@gmail.com>
2019-02-15 11:16:18 +0100

    Added two classes that create a dummy item and a dummy user, for easier testing.

--- src/test/java/propra2/leihOrDie/DummyItemGenerator.java
+++ src/test/java/propra2/leihOrDie/DummyItemGenerator.java
@@ -5,31 +5,27 @@ import propra2.leihOrDie.model.Item;
 import propra2.leihOrDie.model.User;
 
 public class DummyItemGenerator {
-    public DummyItemGenerator() { }
-
-    public Item generateItem(User user) {
+    public Item generateItem(User user){
         String name= "Fahrrad";
         String description = "Stereo Hybrid 160 Action Team 500Wh (2018)";
         int cost = 50;
         int deposit= 100;
         boolean availability = true;
         int availableTime = 10;
-        String location = user.getAddress().getCity();
 
-        Item item = new Item(name, description, cost,deposit, availability, availableTime, location, user);
+        Item item = new Item(name, description, cost,deposit, availability, availableTime, user);
         return item;
     }
 
-    public Item generateAnotherItem(User user) {
+    public Item generateAnotherItem (User user) {
         String name= "Kickbike";
         String description = "Kickbike Cross MAX 29er – Offroad. Farbe: Schwartz";
         int cost = 80;
         int deposit= 120;
         boolean availability = true;
         int availableTime = 14;
-        String location = user.getAddress().getCity();
 
-        Item item = new Item(name, description, cost,deposit, availability, availableTime, location, user);
+        Item item = new Item(name, description, cost,deposit, availability, availableTime, user);
         return item;
     }
 }
--- src/test/java/propra2/leihOrDie/ItemRepositoryTest.java
+++ src/test/java/propra2/leihOrDie/ItemRepositoryTest.java
@@ -40,10 +40,9 @@ public class ItemRepositoryTest {
         int deposit= 100;
         boolean availability = true;
         int availableTime = 10;
-        String location = "Düsseldorf";
         DummyUserGenerator dummyUserGenerator = new DummyUserGenerator();
         User user = dummyUserGenerator.generateUser();
-        Item item = new Item(name, description, cost,deposit, availability, availableTime, location, user);
+        Item item = new Item(name, description, cost,deposit, availability, availableTime, user);
 
         Assert.assertEquals(item.getName(), "Fahrrad");
         Assert.assertEquals(item.getDescription(), "Stereo Hybrid 160 Action Team 500Wh (2018)");
@@ -89,7 +88,6 @@ public class ItemRepositoryTest {
         Assertions.assertThat(firstItemFromRepo.getCost()).isEqualTo(50);
         Assertions.assertThat(firstItemFromRepo.getDeposit()).isEqualTo(100);
         Assertions.assertThat(firstItemFromRepo.getAvailableTime()).isEqualTo(10);
-        Assertions.assertThat(firstItemFromRepo.getLocation()).isEqualTo("Düsseldorf");
 
         Item secondItemFromRepo = itemList.get(0);
         Assertions.assertThat(secondItemFromRepo.getName()).isEqualTo("Kickbike");
@@ -97,7 +95,6 @@ public class ItemRepositoryTest {
         Assertions.assertThat(secondItemFromRepo.getCost()).isEqualTo(80);
         Assertions.assertThat(secondItemFromRepo.getDeposit()).isEqualTo(120);
         Assertions.assertThat(secondItemFromRepo.getAvailableTime()).isEqualTo(14);
-        Assertions.assertThat(secondItemFromRepo.getLocation()).isEqualTo("Düsseldorf");
     }
 
     @Test
