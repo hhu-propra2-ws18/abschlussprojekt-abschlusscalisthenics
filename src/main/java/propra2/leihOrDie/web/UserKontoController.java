c047a8a6c056dac2b2421903913c79587f1e9410
alexstch <alexander.stach@ish.de>
2019-02-19 15:21:36 +0100

    autowired

--- src/main/java/propra2/leihOrDie/web/UserKontoController.java
+++ src/main/java/propra2/leihOrDie/web/UserKontoController.java
@@ -1,11 +1,28 @@
 package propra2.leihOrDie.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import propra2.leihOrDie.dataaccess.ItemRepository;
+import propra2.leihOrDie.dataaccess.LoanRepository;
+import propra2.leihOrDie.dataaccess.PictureRepository;
+import propra2.leihOrDie.dataaccess.UserRepository;
 
 @Controller
 public class UserKontoController {
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private LoanRepository loanRepository;
+
+    @Autowired
+    private PictureRepository pictureRepository;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/Userkonto")
     public String Konto(){
