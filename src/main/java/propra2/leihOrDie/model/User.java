8e65b336b368a6f7815c28802a8e3c707f821ca8
Timo Gatta <timo.gatta@posteo.de>
2019-02-19 10:51:54 +0100

    Add hashed passwords

--- src/main/java/propra2/leihOrDie/model/User.java
+++ src/main/java/propra2/leihOrDie/model/User.java
@@ -1,29 +1,38 @@
 package propra2.leihOrDie.model;
 
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
 import javax.persistence.*;
 import javax.validation.constraints.Size;
+
+import propra2.leihOrDie.WebSecurityConfig;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 
 @Data
 @Entity
 public class User {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
     @Size(max=100)
     @Id
     @Column(name="username")
     private String username;
 
     private String email;
-    private byte[] password;
+    private String password;
 
     @Embedded
     private Address address;
 
     public User() {}
 
-    public User(String username, String email, byte[] password, Address address) {
+    public User(String username, String email, CharSequence password, Address address) {
         this.username = username;
         this.email = email;
-        this.password = password;
+        this.password = passwordEncoder.encode(password);
         this.address = address;
     }
 }
