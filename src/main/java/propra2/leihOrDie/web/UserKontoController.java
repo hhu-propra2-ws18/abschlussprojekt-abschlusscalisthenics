b9f8cb754421f5b213e2cd6b0c6db86db3275cf7
alexstch <alexander.stach@ish.de>
2019-02-20 10:45:45 +0100

    Userkonto Update

--- src/main/java/propra2/leihOrDie/web/UserKontoController.java
+++ src/main/java/propra2/leihOrDie/web/UserKontoController.java
@@ -2,6 +2,7 @@ package propra2.leihOrDie.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import propra2.leihOrDie.dataaccess.ItemRepository;
@@ -37,5 +38,10 @@ public class UserKontoController {
     @GetMapping("Artikelliste")
     public String Artikelliste(){
         return "Artikelliste";
+    }
+
+    private void loadArtikelTabelle(Model model){
+      //  model.addAttribute();
+
     }
 }
