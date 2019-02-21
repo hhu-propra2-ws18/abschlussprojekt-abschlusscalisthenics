630977c44685d5c08ca20515bc0775209bd745d0
tomdevin <t.d.bloemeke@gmail.com>
2019-02-21 13:48:03 +0100

    Create LoanController.java; Coauthor: Timo Gatta (TimoGatta)

--- src/main/java/propra2/leihOrDie/web/LoanController.java
+++ src/main/java/propra2/leihOrDie/web/LoanController.java
@@ -2,10 +2,18 @@ package propra2.leihOrDie.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import propra2.leihOrDie.dataaccess.ItemRepository;
 import propra2.leihOrDie.dataaccess.LoanRepository;
 import propra2.leihOrDie.dataaccess.UserRepository;
+import propra2.leihOrDie.model.Item;
+import propra2.leihOrDie.model.Loan;
+
+import javax.validation.Valid;
+
+import static propra2.leihOrDie.web.ProPayWrapper.reserve;
 
 public class LoanController {
     @Autowired
@@ -15,9 +23,31 @@ public class LoanController {
     @Autowired
     LoanRepository loanRepository;
 
-    @PostMapping("/request")
-    public String requestLoan(Model model) {
+    @PostMapping("/request/{id}")
+    public String requestLoan(Model model, @Valid LoanForm form, @CookieValue(value="SessionID", defaultValue="") String sessionId, @PathVariable Long itemId) {
+
+        //User user = userRepository.findUserByEMail(form.getMail());
+        Item item = itemRepository.findById(itemId).get(0);
+
+        if (!item.isAvailability()) {
+            return "";
+        }
+
+        try {
+            Long propayReservationId = reserve(user.getEmail(), item.getUser().getEmail(), item.getDeposit()).getId();
+        } catch (Exception e) {
+            return "";
+        }
+
+        Loan loan = new Loan("pending", form.getDuration(), user.getEmail(), itemId, propayReservationId);
+        saveLoan(loan);
+
+        item.setAvailability(false);
 
         return "";
+    }
+
+    private void saveLoan(Loan loan) {
+        loanRepository.save(loan);
     }
 }
