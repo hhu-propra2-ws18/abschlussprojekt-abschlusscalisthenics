8e7c8842c4c829f7637fa9a0b38ae6cc3b2fe480
tomdevin <t.d.bloemeke@gmail.com>
2019-02-21 10:37:29 +0100

    Create LoanController.java; Coauthor: Timo Gatta (TimoGatta)

--- src/main/java/propra2/leihOrDie/web/LoanController.java
+++ src/main/java/propra2/leihOrDie/web/LoanController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.PathVariable;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import propra2.leihOrDie.dataaccess.ItemRepository;
 import propra2.leihOrDie.dataaccess.LoanRepository;
