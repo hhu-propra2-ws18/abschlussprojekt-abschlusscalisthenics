05d9988ab43ff90c72db83a9442b38fb544ea03e
serwansj <44338225+serwansj@users.noreply.github.com>
2019-02-22 11:57:44 +0100

    Auth serwansj (#52)
    * Allow users to access item list without login
    
    * Fix typo in item-detail template and add username to template

--- src/main/java/propra2/leihOrDie/web/WebMvcConfig.java
+++ src/main/java/propra2/leihOrDie/web/WebMvcConfig.java
@@ -1,14 +1,10 @@
 package propra2.leihOrDie.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-
-import javax.annotation.PostConstruct;
 import java.util.Arrays;
 
 @Configuration
@@ -27,7 +23,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authenticationInterceptor())
-                .excludePathPatterns(Arrays.asList("/", "/borrowall/*", "/signout"));
+                .excludePathPatterns(Arrays.asList("/", "/borrowall/**", "/borrowall", "/signout"));
 
         registry.addInterceptor(signOutButtonInterceptor())
                 .excludePathPatterns("/signout");
