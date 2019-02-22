43cfffb0cd2901fb2d90047681dde2c2dc04d621
tomdevin <t.d.bloemeke@gmail.com>
2019-02-22 11:51:22 +0100

    update UserController and Loan Repository

--- src/main/java/propra2/leihOrDie/dataaccess/LoanRepository.java
+++ src/main/java/propra2/leihOrDie/dataaccess/LoanRepository.java
@@ -3,6 +3,7 @@ package propra2.leihOrDie.dataaccess;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import propra2.leihOrDie.model.Item;
 import propra2.leihOrDie.model.Loan;
 
 import java.util.List;
@@ -14,6 +15,8 @@ public interface LoanRepository extends CrudRepository<Loan, Long> {
     List<Loan> findLoansOfUser(@Param("username") String username);
 
     @Query("SELECT l FROM Loan l WHERE l.item.id = :itemId")
-
     List<Loan> findLoansOfItem(@Param("itemId") Long itemId);
+
+    @Query("SELECT l FROM Loan l, Item i WHERE l.item.id = :itemId AND l.status = 'pending' AND i.availability = false AND i.id = :itemId")
+    List<Loan> findPendingLoans(@Param("itemId") Long itemId);
 }
