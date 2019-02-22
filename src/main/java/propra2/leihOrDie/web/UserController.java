bc5d414292c9211169ea49199b88aca6a544e11c
tomdevin <36441719+tomdevin@users.noreply.github.com>
2019-02-22 14:52:35 +0100

    Userkonto (#54)
    * update UserKontoController.java
    
    * update UserController and Loan Repository
    
    * update UserController and Loan Repository
    
    * update UserController
    
    * update user.html for Userkonto
    
    * expand DatabaseInitializer
    
    * load attributes into the userpage
    
    * fix bug
    
    * update user.html
    
    * gradle-wrapper
    
    * update UserKontoController.java
    
    * update UserController and Loan Repository
    
    * update UserController and Loan Repository
    
    * update UserController
    
    * update user.html for Userkonto
    
    * expand DatabaseInitializer
    
    * load attributes into the userpage
    
    * fix bug
    
    * update user.html
    
    * gradle-wrapper

---
+++ src/main/java/propra2/leihOrDie/web/UserController.java
@@ -0,0 +1,90 @@
+package propra2.leihOrDie.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import propra2.leihOrDie.dataaccess.ItemRepository;
+import propra2.leihOrDie.dataaccess.LoanRepository;
+import propra2.leihOrDie.dataaccess.SessionRepository;
+import propra2.leihOrDie.model.Item;
+import propra2.leihOrDie.model.Loan;
+import propra2.leihOrDie.model.User;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired
+    private ItemRepository itemRepository;
+    @Autowired
+    private LoanRepository loanRepository;
+    @Autowired
+    SessionRepository sessionRepository;
+
+    @GetMapping("/user/{username}")
+    public String showUser(Model model, @PathVariable String username, @CookieValue(value="SessionID", defaultValue="") String sessionId) {
+        if (!isAuthorized(sessionId, username)) {
+            return "";
+        }
+        model.addAttribute("user", username);
+        model.addAttribute("pendingitems", getPendingItems(username));
+        model.addAttribute("loans", loanRepository.findLoansOfUser(username));
+        model.addAttribute("items", itemRepository.findItemsOfUser(username));
+        return "user";
+    }
+
+    @PostMapping("/user/{username}")
+    public String setStatusOfLoan(Model model, @PathVariable String username, @Valid UserForm form, @CookieValue(value="SessionID", defaultValue="") String sessionId) {
+        if (!isAuthorized(sessionId, username)) {
+            return "user";
+        }
+
+        return "user";
+    }
+
+    private List<Item> collectArtikel(Long[] itemID){
+        List<Item> items = new ArrayList<>();
+        if (itemID == null)
+            return items;
+        for (int i=0; i<itemID.length; i++){
+    //        items.add(ItemRepository.findById(itemID[i].get());
+        }
+        return items;
+    }
+
+    private void loadAusgelieheneArtikel(Model model, Item item){
+        model.addAttribute("id", item.getId());
+        model.addAttribute("name", item.getName());
+    }
+    private void loadArtikelAnfragen(Model model, Item item){
+        model.addAttribute("id", item.getId());
+        model.addAttribute("Name", item.getName());
+    }
+
+    private List<Loan> getPendingItems(String username) {
+        List<Item> itemsOfUser = itemRepository.findItemsOfUser(username);
+        List<Loan> loans = new ArrayList<>();
+        for (Item i: itemsOfUser) {
+            Loan temp = null;
+            temp = loanRepository.findLoansOfItem(i.getId()).get(0);
+            if (temp.getState().equals("pending")) {
+                loans.add(temp);
+            }
+        }
+        return loans;
+    }
+
+    private boolean isAuthorized(String sessionId, String username) {
+        User user = sessionRepository.findUserBySessionCookie(sessionId);
+        if (user.getUsername().equals(username)) {
+            return true;
+        }
+        return false;
+    }
+}
