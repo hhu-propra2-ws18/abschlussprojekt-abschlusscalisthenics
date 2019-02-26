96f8b91e0c2fb4ebf6e9ea938847a3877f1d51da
tomdevin <t.d.bloemeke@gmail.com>
2019-02-26 10:52:33 +0100

    Update Loan and UserConrtoller and create Responsebuilder

--- src/main/java/propra2/leihOrDie/web/LoanController.java
+++ src/main/java/propra2/leihOrDie/web/LoanController.java
@@ -1,7 +1,6 @@
 package propra2.leihOrDie.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -32,6 +31,8 @@ public class LoanController {
     @Autowired
     SessionRepository sessionRepository;
 
+    private ResponseBuilder responseBuilder = new ResponseBuilder();
+
     @PostMapping(value="/request/{itemId}")
     @ResponseBody
     public ResponseEntity requestLoan(Model model, @Valid LoanForm form, @CookieValue(value="SessionID", defaultValue="") String sessionId, @PathVariable Long itemId) {
@@ -39,26 +40,26 @@ public class LoanController {
         Item item = itemRepository.findById(itemId).get();
 
         if (form.getLoanDuration() == 0) {
-            return createErrorResponse("Die minimale Ausleihdauer beträgt einen Tag.");
+            return responseBuilder.createErrorResponse("Die minimale Ausleihdauer beträgt einen Tag.");
         }
 
         if (!item.isAvailability()) {
-           return createErrorResponse("Dieser Gegenstand ist nicht verfügbar.");
+           return responseBuilder.createErrorResponse("Dieser Gegenstand ist nicht verfügbar.");
         }
 
         if (form.getLoanDuration() > item.getAvailableTime()) {
-            return createErrorResponse("Die maximale Ausleihdauer ist überschritten.");
+            return responseBuilder.createErrorResponse("Die maximale Ausleihdauer ist überschritten.");
         }
 
         if (item.getUser() == user) {
-            return createErrorResponse("Du kannst deinen eigenen Gegenstand nicht ausleihen.");
+            return responseBuilder.createErrorResponse("Du kannst deinen eigenen Gegenstand nicht ausleihen.");
         }
 
         Long proPayReservationId;
         try {
             proPayReservationId = reserve(user.getEmail(), item.getUser().getEmail(), item.getDeposit()).getId();
         } catch (Exception e) {
-            return createErrorResponse("ProPay Fehler");
+            return responseBuilder.createErrorResponse("ProPay Fehler");
         }
 
         Loan loan = new Loan("pending", form.getLoanDuration(), user, item, proPayReservationId);
@@ -66,8 +67,7 @@ public class LoanController {
 
         item.setAvailability(false);
         itemRepository.save(item);
-
-        return createSuccessResponse("Eine Anfrage wurde gesendet.");
+        return responseBuilder.createSuccessResponse("Eine Anfrage wurde gesendet.");
     }
 
     @PostMapping("/request/accept/{loanId}")
@@ -75,13 +75,13 @@ public class LoanController {
         Loan loan = loanRepository.findById(loanId).get();
 
         if (!isAuthorized(sessionId, loan.getItem().getId())) {
-            return createErrorResponse("Du bist nicht authorisiert diese Aktion durchzuführen");
+            return responseBuilder.createUnauthorizedResponse();
         }
 
         loan.setState("accepted");
         loanRepository.save(loan);
 
-        return createSuccessResponse("Bestätigt.");
+        return responseBuilder.createSuccessResponse("Bestätigt.");
     }
 
     @PostMapping("/request/decline/{loanId}")
@@ -90,7 +90,7 @@ public class LoanController {
         Item item = itemRepository.findById(loan.getItem().getId()).get();
 
         if (!isAuthorized(sessionId, loan.getItem().getId())) {
-            return createErrorResponse("Du bist nicht authorisiert diese Aktion durchzuführen");
+            return responseBuilder.createUnauthorizedResponse();
         }
 
         loan.setState("declined");
@@ -99,19 +99,11 @@ public class LoanController {
         item.setAvailability(true);
         itemRepository.save(item);
 
-        return createSuccessResponse("Bestätigt.");
+        return responseBuilder.createSuccessResponse("Bestätigt.");
     }
 
     private void saveLoan(Loan loan) {
         loanRepository.save(loan);
-    }
-
-    private ResponseEntity createErrorResponse(String errorMessage) {
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
-    }
-
-    private ResponseEntity createSuccessResponse(String successMessage) {
-        return new ResponseEntity<>(successMessage, HttpStatus.OK);
     }
 
     private boolean isAuthorized(String sessionId, Long itemId) {
