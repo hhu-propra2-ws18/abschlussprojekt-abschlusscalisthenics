1e65f52e7577d74d7bab0cdd80b6c18462f75191
Timo Gatta <timo.gatta@posteo.de>
2019-02-27 13:13:17 +0100

    Fix merge error. I'm looking at you claudia

--- src/main/java/propra2/leihOrDie/web/LoanController.java
+++ src/main/java/propra2/leihOrDie/web/LoanController.java
@@ -110,10 +110,9 @@ public class LoanController {
 
     @PostMapping("/request/activate/{loanId}")
     public ResponseEntity changeStatusToActive(Model model, @PathVariable Long loanId, @CookieValue(value="SessionID", defaultValue="") String sessionId) {
-        User user = sessionRepository.findUserBySessionCookie(sessionId);
         Loan loan = loanRepository.findById(loanId).get();
 
-        if (!isAuthorized(sessionId, loan.getItem().getUser())) {
+        if (!authorizationHandler.isAuthorized(sessionId, loan.getItem().getUser())) {
             return responseBuilder.createUnauthorizedResponse();
         }
 
@@ -129,7 +128,7 @@ public class LoanController {
         User user = sessionRepository.findUserBySessionCookie(sessionId);
         Loan loan = loanRepository.findById(loanId).get();
 
-        if (!isAuthorized(sessionId, loan.getItem().getUser())) {
+        if (!authorizationHandler.isAuthorized(sessionId, loan.getItem().getUser())) {
             return responseBuilder.createUnauthorizedResponse();
         }
 
@@ -150,116 +149,4 @@ public class LoanController {
 
         return responseBuilder.createSuccessResponse("Erfolgreich!");
     }
-
-    @PostMapping("/conflict/open/{loanId}")
-    public ResponseEntity openConflict(Model model, @PathVariable Long loanId,
-                                       @CookieValue(value="SessionID", defaultValue="") String sessionId) {
-        if (!loanRepository.findById(loanId).isPresent()) {
-            return responseBuilder.createBadRequestResponse("Die angefragte Ausleihe existiert nicht.");
-        }
-
-        Loan loan = loanRepository.findById(loanId).get();
-
-        if (!isAuthorized(sessionId, loan.getItem()) || !isAuthorized(sessionId, loan.getUser())) {
-            return responseBuilder.createUnauthorizedResponse();
-        }
-
-        if (!loan.getState().equals("active")) {
-            return responseBuilder.createBadRequestResponse("Status der Ausleihe ist nicht \"aktiv\".");
-        }
-
-        loan.setState("conflict");
-        loanRepository.save(loan);
-
-        loan.getItem().setAvailability(false);
-        itemRepository.save(loan.getItem());
-
-        String message = "Konflikt wurde erstellt. Bitte sende eine Email mit der genauen Beschreibung Deines Problems " +
-                "und dem Betreff \"" + sessionRepository.findUserBySessionCookie(sessionId).getUsername() + " - " +
-                loan.getId().toString() + "\" an conflict@leihordie.de";
-
-        return responseBuilder.createSuccessResponse(message);
-    }
-
-    @PostMapping("/conflict/solve/{loanId}")
-    public ResponseEntity solveConflict(Model model, ConflictForm form, @PathVariable Long loanId,
-                                        @CookieValue(value="SessionID", defaultValue="") String sessionId) {
-        if (!isAdmin(sessionId)) {
-            return responseBuilder.createUnauthorizedResponse();
-        }
-
-        if (!loanRepository.findById(loanId).isPresent()) {
-            return responseBuilder.createBadRequestResponse("Loan " + loanId + " exisitert nicht.");
-        }
-
-        Loan loan = loanRepository.findById(loanId).get();
-
-        String covenanteeEmail = form.getCovenanteeEmail();
-
-        if (userRepository.findUserByEMail(covenanteeEmail).isEmpty()) {
-            return responseBuilder.createBadRequestResponse("User " + covenanteeEmail + " existiert nicht.");
-        }
-
-
-        User convenantee = userRepository.findUserByEMail(covenanteeEmail).get(0);
-        User lendingUser = userRepository.findUserByEMail(convenantee.getEmail()).get(0);
-
-        if (convenantee.getEmail().equals(loan.getUser().getEmail())) {
-            try {
-                freeReservationOfUser(lendingUser.getEmail(), loan.getProPayReservationId());
-            } catch (Exception e) {
-                return responseBuilder.createBadRequestResponse("ProPay Fehler");
-            }
-
-        } else if(convenantee.getEmail().equals(loan.getItem().getUser().getEmail())) {
-            try {
-                punishAccount(lendingUser.getEmail(), loan.getProPayReservationId());
-            } catch (Exception e) {
-                return responseBuilder.createBadRequestResponse("ProPay Fehler");
-            }
-
-        } else {
-            return responseBuilder.createBadRequestResponse("User " + covenanteeEmail + " steht nicht im Kontext mit Loan " +
-                    loan.getId() + ".");
-        }
-
-        loan.setState("completed");
-        loanRepository.save(loan);
-
-        Item item = loan.getItem();
-        item.setAvailability(false);
-        itemRepository.save(item);
-
-        return responseBuilder.createSuccessResponse("Konflikt wurde gelöst.");
-    }
-
-    @GetMapping("/conflict/admin")
-    public String getOpenConflicts(Model model, @CookieValue(value="SessionID", defaultValue="") String sessionId) {
-        if (!isAdmin(sessionId)) {
-            return "redirect:/";
-        }
-
-        List<Loan> openConflicts = loanRepository.findLoansByState("conflict");
-        model.addAttribute("conflicts", openConflicts);
-
-        return "conflict-list";
-    }
-
-    private boolean isAuthorized(String sessionId, Item item) {
-        User user = sessionRepository.findUserBySessionCookie(sessionId);
-
-        return user.getUsername().equals(item.getUser().getUsername());
-    }
-
-    private boolean isAuthorized(String sessionId, User user) {
-        User sessionUser = sessionRepository.findUserBySessionCookie(sessionId);
-
-        return sessionUser.getUsername().equals(user.getUsername());
-    }
-
-    private boolean isAdmin(String sessionId) {
-        return sessionRepository.findUserBySessionCookie(sessionId).getRole().equals("ADMIN");
-    }
-
-
 }
