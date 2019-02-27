a474e2d096a570f4ef5301e5dbca4eb81cdd0803
tomdevin <t.d.bloemeke@gmail.com>
2019-02-27 10:50:29 +0100

    Create Buy model

--- src/main/java/propra2/leihOrDie/model/Buy.java
+++ src/main/java/propra2/leihOrDie/model/Buy.java
@@ -18,7 +18,9 @@ public class Buy {
 
     private int purchasePrice;
     private String status;
-    
+
+    @ManyToOne
+    @JoinColumn(name="username")
     private User buyer;
 
     public Buy() {}
