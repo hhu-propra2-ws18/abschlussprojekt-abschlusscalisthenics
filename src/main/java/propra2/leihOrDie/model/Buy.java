f0249cee898ef65fbf01c7cd6f39ab017d4ad3ae
tomdevin <36441719+tomdevin@users.noreply.github.com>
2019-02-27 12:57:51 +0100

    Buy function (#66)
    * Create Buy model
    
    * Create BuyRepository and update Buy model
    
    * Add BuyRepository
    
    * Update BuyRepository and create BuyController and BuyForm
    
    * Update BuyController

---
+++ src/main/java/propra2/leihOrDie/model/Buy.java
@@ -0,0 +1,32 @@
+package propra2.leihOrDie.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+public class Buy {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="buy_id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="item_id")
+    private Item item;
+
+    private int purchasePrice;
+    private String status;
+
+    private User buyer;
+
+    public Buy() {}
+
+    public Buy(Item item, int purchasePrice, String status, User buyer) {
+        this.item = item;
+        this.purchasePrice = purchasePrice;
+        this.status = status;
+        this.buyer = buyer;
+    }
+}
