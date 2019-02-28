2e8ec4f489eb28978e64aff5feca455c3f1eae58
mofoku <lata222@gmail.com>
2019-02-28 11:24:29 +0100

    test class for the user controler

--- src/test/java/propra2/leihOrDie/UserControllerTest.java
+++ src/test/java/propra2/leihOrDie/UserControllerTest.java
@@ -2,11 +2,9 @@ package propra2.leihOrDie;
 
 
 import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -14,19 +12,15 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import propra2.leihOrDie.dataaccess.*;
 import propra2.leihOrDie.model.Item;
-import propra2.leihOrDie.model.Picture;
 import propra2.leihOrDie.model.Session;
 import propra2.leihOrDie.model.User;
-
 import javax.servlet.http.Cookie;
-import java.io.File;
-import java.util.List;
-
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -45,23 +39,6 @@ public class UserControllerTest {
     private TransactionRepository transactionRepository;
     @Autowired
     UserRepository userRepository;
-
-
-    @Before
-    public void setUp() {
-        /*DummyUserGenerator dummyUserGenerator = new DummyUserGenerator();
-        User user =dummyUserGenerator.generateUser();
-        userRepository.save(user);
-
-        DummyItemGenerator dummyItemGenerator = new DummyItemGenerator();
-        Item item1 = dummyItemGenerator.generateItem(user);
-        Item item2 = dummyItemGenerator.generateAnotherItem(user);
-        itemRepository.save(item1);
-        itemRepository.save(item2);
-
-        sessionRepository.save(new Session("1", user));*/
-
-    }
 
     @After
     public void tearDown() {
@@ -82,7 +59,6 @@ public class UserControllerTest {
 
         sessionRepository.save(new Session("1", user));
 
-
         mvc.perform(get("/myaccount")
                 .cookie(new Cookie("SessionID", "1")))
                 .andExpect(MockMvcResultMatchers.status().isOk())
@@ -93,5 +69,53 @@ public class UserControllerTest {
                                 hasProperty("cost", is(item.getCost())),
                                 hasProperty("deposit", is(item.getDeposit())),
                                 hasProperty("description", is(item.getDescription()))))));
+    }
+
+    @Test
+    public void testUserNotLoggedIn() throws Exception {
+        mvc.perform(get("/myaccount"))
+                .andExpect(MockMvcResultMatchers
+                        .redirectedUrl("/login"));
+    }
+
+    @Test
+    public void testShowPropay() throws Exception {
+        DummyUserGenerator dummyUserGenerator = new DummyUserGenerator();
+        User user =dummyUserGenerator.generateUser();
+        userRepository.save(user);
+
+        DummyItemGenerator dummyItemGenerator = new DummyItemGenerator();
+        Item item = dummyItemGenerator.generateItem(user);
+        itemRepository.save(item);
+
+        sessionRepository.save(new Session("1", user));
+
+        mvc.perform(get("/myaccount/propay")
+                .cookie(new Cookie("SessionID", "1")))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(model().attribute("bankBalance", 0.0))
+                .andExpect(model().attribute("items", isEmptyOrNullString()));
+    }
+
+    @Test
+    public void testDoTransaction() throws Exception {
+        DummyUserGenerator dummyUserGenerator = new DummyUserGenerator();
+        User user =dummyUserGenerator.generateUser();
+        userRepository.save(user);
+
+        DummyItemGenerator dummyItemGenerator = new DummyItemGenerator();
+        Item item = dummyItemGenerator.generateItem(user);
+        itemRepository.save(item);
+
+        sessionRepository.save(new Session("1", user));
+
+        mvc.perform(post("/myaccount/propay")
+                .cookie(new Cookie("SessionID", "1"))
+                .param("amount", "200.0"))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+        mvc.perform(get("/myaccount/propay")
+                .cookie(new Cookie("SessionID", "1")))
+                .andExpect(model().attribute("bankBalance", 200.0))
+                .andExpect(model().attribute("items", isEmptyOrNullString()));
     }
 }
