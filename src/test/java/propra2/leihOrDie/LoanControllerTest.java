074ff12affe743debe0de32e90af3284c1d28bec
mofoku <lata222@gmail.com>
2019-02-28 15:23:48 +0100

    loan controller test

---
+++ src/test/java/propra2/leihOrDie/LoanControllerTest.java
@@ -0,0 +1,72 @@
+package propra2.leihOrDie;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import propra2.leihOrDie.dataaccess.*;
+import propra2.leihOrDie.model.Item;
+import propra2.leihOrDie.model.Session;
+import propra2.leihOrDie.model.User;
+
+import javax.servlet.http.Cookie;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class LoanControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
+    @Autowired
+    PictureRepository pictureRepository;
+
+    @Autowired
+    SessionRepository sessionRepository;
+
+    @Autowired
+    LoanRepository loanRepository;
+
+    @After
+    public void tearDown() {
+        pictureRepository.deleteAll();
+        loanRepository.deleteAll();
+        sessionRepository.deleteAll();
+        itemRepository.deleteAll();
+        userRepository.deleteAll();
+    }
+
+    @Test
+    public void testRequestLoan() throws Exception {
+        DummyUserGenerator dummyUserGenerator = new DummyUserGenerator();
+        User user1 = dummyUserGenerator.generateUser();
+        User user2 = dummyUserGenerator.generateUser();
+        userRepository.save(user1);
+        userRepository.save(user2);
+        sessionRepository.save(new Session("1", user2));
+        DummyItemGenerator dummyItemGenerator = new DummyItemGenerator();
+        Item item = dummyItemGenerator.generateItem(user1);
+        itemRepository.save(item);
+
+        mvc.perform(post("/request/" + item.getId())
+                .cookie(new Cookie("SessionID", "1")))
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/borrowall/"));
+
+        Assert.assertEquals(loanRepository.findLoansOfUser(user2.getUsername()).size(), 1);
+    }
+}
