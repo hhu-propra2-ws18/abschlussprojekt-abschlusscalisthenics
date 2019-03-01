584a3db56094afb566d3bf8ae83012a8104f6cbd
mofoku <lata222@gmail.com>
2019-03-01 11:49:47 +0100

    refactor tests

--- src/test/java/propra2/leihOrDie/LoanControllerTest.java
+++ src/test/java/propra2/leihOrDie/LoanControllerTest.java
@@ -2,7 +2,6 @@ package propra2.leihOrDie;
 
 import org.junit.*;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
